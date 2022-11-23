dcf46a241c8fd5da3f2b03d2d564ff4d7dde26ca
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-11-23 20:13:18 +0100

    update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,14 +2,14 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import Logo from '../public/assets/images/Logo.png'
-import {AiOutlineClose} from 'react-icons/ai'
+import {AiOutlineClose,AiOutlineMenu} from 'react-icons/ai'
 
 const Navbar = () => {
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
         <div className="flex justify-between items-center w-full h-full px-2
         2xl:px-16">
-            <Image src={Logo} alt='' width='123' height='50'></Image>
+            <Image src={Logo} alt='' width='123' height='50'/>
 
 
             <div>
@@ -35,11 +35,20 @@ const Navbar = () => {
                 </Link>
             </ul>
 
-            <div>
+            <div className='md:hidden'>
                 <AiOutlineClose/>
+                <AiOutlineMenu size={25}/>
+            </div>
+        </div>
+        </div>
+
+        <div className="fixed left-0 top-0 w-full h-screen bg-black/70">
+            <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] m:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500">
+                <div>
+                    <Image src={Logo} alt='/' width='87' height='35'/>
+                </div>
             </div>
 
-        </div>
         </div>
     </div>
   )
