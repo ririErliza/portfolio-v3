17d9d5a4f0ca4a959dc0f83e1d1ebbfbb21a1119
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-11-24 20:49:48 +0100

    update

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import ProjectItem from "./ProjectItem"
+import img1 from '../public/assets/projects/web-reviver.png'
+import img2 from '../public/assets/projects/reviveradmin-page.png'
+import img3 from '../public/assets/projects/reviver-merchandise-checkout.png'
+
 
 const Projects = () => {
   return (
@@ -8,6 +12,10 @@ const Projects = () => {
             <h2>What I have Built</h2>
             <div className="grid md:grid-cols-2 gap-8">
 
+                
+            <ProjectItem title='NGO Web App' bgImg={img1} projectUrl='/ngoweb'/>
+            <ProjectItem title='NGO Admin Panel' bgImg={img2} projectUrl='/ngoadmin'/>
+            <ProjectItem title='Merchandise Shop' bgImg={img3} projectUrl='/shop'/>
             </div>
 
         </div>
