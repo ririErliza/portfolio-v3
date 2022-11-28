5dd745bc63c6c083d300c2548118d180bbd4b818
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-11-28 14:13:21 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import About from '../components/About'
 import Contact from '../components/Contact'
 import Main from '../components/Main'
-import Navbar from '../components/Navbar'
+// import Navbar from '../components/Navbar'
 import Projects from '../components/Projects'
 import Skills from '../components/Skills'
 
@@ -17,7 +17,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-    <Navbar/>
+    {/* <Navbar/> */}
     <Main/>
     <About/>
     <Skills/>
