d7c5f4d83da6181f1ff933f7a3ac71bfc7554012
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-11-28 14:08:07 +0100

    update

--- pages/reviverngo.jsx
+++ pages/reviverngo.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const reviverngo = () => {
   return (
-    <div>reviverngo</div>
+    <div className='w-full'>
+        <div className="w-screen h-[30vh] lg:w-[40vh] relative">
+            <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10'>
+                
+            </div>
+
+        </div>
+        
+    </div>
   )
 }
 
