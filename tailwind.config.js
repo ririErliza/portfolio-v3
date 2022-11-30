b84fd3231cfde89e1b71e0fdd9501466f5e85b1e
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-11-30 14:12:30 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   "./components/**/*.{js,ts,jsx,tsx}",
 ],
   theme: {
-    extend: {},
+    extend: {
+      animation:{
+        'spin-slow': 'spin 30s linear infinite',
+      }
+    },
   },
   plugins: [],
 }
